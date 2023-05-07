553b8320737b64d7f9706e2afbc9598dda25879d
IBassaoud <ismail.bassaoud@gmail.com>
2023-05-07 16:10:41 +0200

    :rocket: Update production build for GitHub Pages deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCoFK8wf2T_Cjmq1HGd78wMkq1r1e6LumY',
+    authDomain: 'rorr-1124e.firebaseapp.com',
+    projectId: 'rorr-1124e',
+    storageBucket: 'rorr-1124e.appspot.com',
+    messagingSenderId: '1023753832713',
+    appId: '1:1023753832713:web:b5f7e7d0960d2477126bd8',
+    measurementId: 'G-0Q1J784GZB',
+  },
 };
