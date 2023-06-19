92e590fbb964d712f20fed0e354c4b062773349d
martin-rabaud <rabaud.martin.e@gmail.com>
2023-06-19 10:24:37 +0200

    refacto: put the website inside a front folder

--- front/src/app/pages/publish-roadmap/publish-roadmap.component.ts
+++ front/src/app/pages/publish-roadmap/publish-roadmap.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { SnackbarService } from 'src/app/core/services/snackbar.service';
 
@@ -13,11 +14,18 @@ export class PublishRoadmapComponent implements OnInit {
   boardUrl: string = '';
   @ViewChild('boardLink', { static: false }) boardLink!: ElementRef<HTMLAnchorElement>;
 
+  messageForm: FormGroup;
+
   constructor(
+    private formBuilder: FormBuilder,
     private route: ActivatedRoute,
     private router: Router,
     private snackbarService: SnackbarService
-  ) { }
+  ) {
+    this.messageForm = this.formBuilder.group({
+      message: ''
+    })
+   }
 
   ngOnInit(): void {
     this.loading = true;
@@ -47,5 +55,11 @@ export class PublishRoadmapComponent implements OnInit {
     }).catch((error) => {
       this.snackbarService.showError('Failed to copy link to clipboard');
     });
+  }
+
+  publishChange() {
+    if (this.messageForm.valid) {
+      // Send email
+    }
   }
 }
